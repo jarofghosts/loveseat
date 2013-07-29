0456073e364ea5955be5011d9abb5a75afdac785
jesse keane <me@jessekeane.me>
2013-07-30 02:02:54 +0400

    im a dummy

--- test/test.js
+++ test/test.js
@@ -11,36 +11,41 @@ db.create(function (err, res) {
   db.check(function (err, res) {
     assert.ok(!err);
     assert.equal(res.db_name, 'test');
+    doNext();
   });
 });
-
-db.insert({ "hello": "world" }, function (err, res) {
-  var id = res.id, rev = res.rev;
-  assert.ok(id && rev);
-  db.get(id, function (err, res) {
-    assert.equal(res.hello, "world");
-    db.insert(id, { "hello": "dolly", "_rev": rev }, function (err, res) {
-      assert.ok(res.ok);
-      rev = res.rev;
-      db.get(id, function (err, res) {
-        assert.equal(res.hello, "dolly");
-        db.destroy(id, res._rev, function (err, res) {
-          assert.ok(res.ok);
-          db.get(id, function (err, res) {
-            assert.equal(res.error, "not_found");
-            assert.equal(res.reason, "deleted");
+function doNext() {
+  db.insert({ "hello": "world" }, function (err, res) {
+    var id = res.id, rev = res.rev;
+    assert.ok(id && rev);
+    db.get(id, function (err, res) {
+      assert.equal(res.hello, "world");
+      db.insert(id, { "hello": "dolly", "_rev": rev }, function (err, res) {
+        assert.ok(res.ok);
+        rev = res.rev;
+        db.get(id, function (err, res) {
+          assert.equal(res.hello, "dolly");
+          db.destroy(id, res._rev, function (err, res) {
+            assert.ok(res.ok);
+            db.get(id, function (err, res) {
+              assert.equal(res.error, "not_found");
+              assert.equal(res.reason, "deleted");
+              doLast();
+            });
           });
         });
       });
     });
   });
-});
 
-db.insert('loveseat-test', { "hey": "now" }, function (err, res) {
-  var rev = res.rev;
-  assert.ok(rev);
-  db.get('loveseat-test', function (err, res) {
-    assert.equal(res.hey, "now");
-    db.destroy('loveseat-test', rev);
+}
+function doLast() {
+  db.insert('loveseat-test', { "hey": "now" }, function (err, res) {
+    var rev = res.rev;
+    assert.ok(rev);
+    db.get('loveseat-test', function (err, res) {
+      assert.equal(res.hey, "now");
+      db.destroy('loveseat-test', rev);
+    });
   });
-});
+}
