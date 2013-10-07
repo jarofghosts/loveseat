a1ac91a21ac914d921150743f01c973ef166a30c
jesse keane <me@jessekeane.me>
2013-10-07 08:17:47 -0700

    cursed ;

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var request = require('request');
+var request = require('request')
 
 function Loveseat(options) {
 
   if (!(this instanceof Loveseat)) return new Loveseat(options);
   
-  options = options || {};
+  options = options || {}
   
   this.settings = {
     url: options.url || 'http://localhost:5984/',
     db: options.db || 'test'
-  };
+  }
 
-  return this;
+  return this
 }
 
 Loveseat.prototype.makeRequest = function (method, url, data, callback) {
@@ -19,46 +19,46 @@ Loveseat.prototype.makeRequest = function (method, url, data, callback) {
   var options = {
     url: this.settings.url + this.settings.db + '/' + url,
     method: method
-  };
+  }
 
-  if (data && (method == 'POST' || method == 'PUT')) options.json = data;
-  else if (data) options.qs = data;
+  if (data && (method == 'POST' || method == 'PUT')) options.json = data
+  else if (data) options.qs = data
 
   request(options, function (err, res, body) {
-    if (err) return callback && callback(err);
+    if (err) return callback && callback(err)
     if (res.statusCode == 409) return callback && callback(JSON.stringify(body));
 
-    callback && callback(null, (options.json || !body.length ? body : JSON.parse(body)));
-  });
+    callback && callback(null, (options.json || !body.length ? body : JSON.parse(body)))
+  })
 
-};
+}
 
 Loveseat.prototype.create = function (callback) {
-  this.makeRequest('PUT', '', null, callback);
-};
+  this.makeRequest('PUT', '', null, callback)
+}
 
 Loveseat.prototype.get = function (docId, callback) {
-  this.makeRequest('GET', docId, null, callback);
-};
+  this.makeRequest('GET', docId, null, callback)
+}
 
 Loveseat.prototype.destroy = function (docId, rev, callback) {
-  this.makeRequest('DELETE', docId, { rev: rev }, callback);
-};
+  this.makeRequest('DELETE', docId, { rev: rev }, callback)
+}
 
 Loveseat.prototype.check = function (callback) {
-  this.makeRequest('HEAD', '', null, callback);
-};
+  this.makeRequest('HEAD', '', null, callback)
+}
 
 Loveseat.prototype.insert = function (docId, doc, callback) {
-  var method = 'PUT';
+  var method = 'PUT'
 
   if (typeof docId !== 'string') {
-    callback = doc;
-    doc = docId;
-    docId = '';
-    method = 'POST';
+    callback = doc
+    doc = docId
+    docId = ''
+    method = 'POST'
   }
   this.makeRequest(method, docId, doc, callback);
-};
+}
 
-exports.Loveseat = Loveseat;
+exports.Loveseat = Loveseat
