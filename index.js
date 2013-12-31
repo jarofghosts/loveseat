df07d2fd266ab2a3a54d215385fe5c546d5dd536
jesse keane <me@jessekeane.me>
2013-12-31 11:35:47 -0800

    ewww

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var request = require('request')
 
+exports.Loveseat = Loveseat
+
 function Loveseat(options) {
 
   if (!(this instanceof Loveseat)) return new Loveseat(options)
@@ -14,8 +16,9 @@ function Loveseat(options) {
   return this
 }
 
-Loveseat.prototype.makeRequest = function (method, url, data, callback) {
+Loveseat.prototype.makeRequest = Loveseat$makeRequest
 
+function Loveseat$makeRequest(method, url, data, callback) {
   var options = {
     url: this.settings.url + this.settings.db + '/' + url,
     method: method
@@ -24,9 +27,11 @@ Loveseat.prototype.makeRequest = function (method, url, data, callback) {
   if (data && (method == 'POST' || method == 'PUT')) options.json = data
   else if (data) options.qs = data
 
-  request(options, function (err, res, body) {
+  request(options, onrequest)
+    
+  function onrequest(err, res, body) {
     if (err) return callback && callback(err)
-    if (res.statusCode == 409) {
+    if (res.statusCode === 409) {
       return callback && callback(JSON.stringify(body))
     }
 
@@ -37,23 +42,23 @@ Loveseat.prototype.makeRequest = function (method, url, data, callback) {
 
 }
 
-Loveseat.prototype.create = function (callback) {
+Loveseat.prototype.create = function Loveseat$create(callback) {
   this.makeRequest('PUT', '', null, callback)
 }
 
-Loveseat.prototype.get = function (docId, callback) {
+Loveseat.prototype.get = function Loveseat$get(docId, callback) {
   this.makeRequest('GET', docId, null, callback)
 }
 
-Loveseat.prototype.destroy = function (docId, rev, callback) {
+Loveseat.prototype.destroy = function Loveseat$destroy(docId, rev, callback) {
   this.makeRequest('DELETE', docId, { rev: rev }, callback)
 }
 
-Loveseat.prototype.check = function (callback) {
+Loveseat.prototype.check = function Loveseat$check(callback) {
   this.makeRequest('HEAD', '', null, callback)
 }
 
-Loveseat.prototype.insert = function (docId, doc, callback) {
+Loveseat.prototype.insert = function Loveseat$insert(docId, doc, callback) {
   var method = 'PUT'
 
   if (typeof docId !== 'string') {
@@ -62,7 +67,6 @@ Loveseat.prototype.insert = function (docId, doc, callback) {
     docId = ''
     method = 'POST'
   }
+
   this.makeRequest(method, docId, doc, callback)
 }
-
-exports.Loveseat = Loveseat
--- test/test.js
+++ test/test.js
@@ -1,50 +1,50 @@
 var Loveseat = require('../index.js').Loveseat,
   assert = require('assert'),
-  db;
+  db
 
 assert.doesNotThrow(function () {
-  db = new Loveseat();
-});
+  db = new Loveseat()
+})
 
 db.create(function (err, res) {
-  assert.ok(!err);
+  assert.ok(!err)
   db.check(function (err, res) {
-    assert.ok(!err);
-    doNext();
-  });
-});
+    assert.ok(!err)
+    doNext()
+  })
+})
 function doNext() {
   db.insert({ hello: 'world' }, function (err, res) {
-    var id = res.id, rev = res.rev;
-    assert.ok(id && rev);
+    var id = res.id, rev = res.rev
+    assert.ok(id && rev)
     db.get(id, function (err, res) {
-      assert.equal(res.hello, 'world');
+      assert.equal(res.hello, 'world')
       db.insert(id, { hello: 'dolly', '_rev': rev }, function (err, res) {
-        assert.ok(res.ok);
-        rev = res.rev;
+        assert.ok(res.ok)
+        rev = res.rev
         db.get(id, function (err, res) {
-          assert.equal(res.hello, 'dolly');
+          assert.equal(res.hello, 'dolly')
           db.destroy(id, res._rev, function (err, res) {
-            assert.ok(res.ok);
+            assert.ok(res.ok)
             db.get(id, function (err, res) {
-              assert.equal(res.error, 'not_found');
-              assert.equal(res.reason, 'deleted');
-              doLast();
-            });
-          });
-        });
-      });
-    });
-  });
+              assert.equal(res.error, 'not_found')
+              assert.equal(res.reason, 'deleted')
+              doLast()
+            })
+          })
+        })
+      })
+    })
+  })
 
 }
 function doLast() {
   db.insert('loveseat-test', { hey: 'now' }, function (err, res) {
-    var rev = res.rev;
-    assert.ok(rev);
+    var rev = res.rev
+    assert.ok(rev)
     db.get('loveseat-test', function (err, res) {
-      assert.equal(res.hey, 'now');
-      db.destroy('loveseat-test', rev);
-    });
-  });
+      assert.equal(res.hey, 'now')
+      db.destroy('loveseat-test', rev)
+    })
+  })
 }
