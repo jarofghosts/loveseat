d8683433ae7f63279088a0087af322f5d2589147
jesse keane <me@jessekeane.me>
2014-04-15 22:03:03 -0700

    cleanup

--- index.js
+++ index.js
@@ -3,69 +3,68 @@ var request = require('request')
 exports.Loveseat = Loveseat
 
 function Loveseat(options) {
-
-  if (!(this instanceof Loveseat)) return new Loveseat(options)
+  if(!(this instanceof Loveseat)) return new Loveseat(options)
   
   options = options || {}
   
   this.settings = {
-    url: options.url || 'http://localhost:5984/',
-    db: options.db || 'test'
+      url: options.url || 'http://localhost:5984/'
+    , db: options.db || 'test'
   }
 
   return this
 }
 
-Loveseat.prototype.makeRequest = Loveseat$makeRequest
+Loveseat.prototype.make_request = Loveseat$make_request
 
-function Loveseat$makeRequest(method, url, data, callback) {
+function Loveseat$make_request(method, url, data, callback) {
   var options = {
     url: this.settings.url + this.settings.db + '/' + url,
     method: method
   }
 
-  if (data && (method == 'POST' || method == 'PUT')) options.json = data
-  else if (data) options.qs = data
+  if(data && (method == 'POST' || method == 'PUT')) options.json = data
+  else if(data) options.qs = data
 
   request(options, onrequest)
     
   function onrequest(err, res, body) {
-    if (err) return callback && callback(err)
-    if (res.statusCode === 409) {
+    if(err) return callback && callback(err)
+    if(res.statusCode === 409) {
       return callback && callback(JSON.stringify(body))
     }
 
-    if (callback) {
+    if(callback) {
       callback(null, (options.json || !body.length ? body : JSON.parse(body)))
     }
   }
 }
 
 Loveseat.prototype.create = function Loveseat$create(callback) {
-  this.makeRequest('PUT', '', null, callback)
+  this.make_request('PUT', '', null, callback)
 }
 
-Loveseat.prototype.get = function Loveseat$get(docId, callback) {
-  this.makeRequest('GET', docId, null, callback)
+Loveseat.prototype.get = function Loveseat$get(doc_id, callback) {
+  this.make_request('GET', doc_id, null, callback)
 }
 
-Loveseat.prototype.destroy = function Loveseat$destroy(docId, rev, callback) {
-  this.makeRequest('DELETE', docId, { rev: rev }, callback)
+Loveseat.prototype.destroy = function Loveseat$destroy(doc_id, rev, callback) {
+  this.make_request('DELETE', doc_id, { rev: rev }, callback)
 }
 
 Loveseat.prototype.check = function Loveseat$check(callback) {
-  this.makeRequest('HEAD', '', null, callback)
+  this.make_request('HEAD', '', null, callback)
 }
 
-Loveseat.prototype.insert = function Loveseat$insert(docId, doc, callback) {
+Loveseat.prototype.insert = function Loveseat$insert(doc_id, doc, callback) {
   var method = 'PUT'
 
-  if (typeof docId !== 'string') {
+  if(typeof doc_id !== 'string') {
     callback = doc
-    doc = docId
-    docId = ''
+    doc = doc_id
+    doc_id = ''
     method = 'POST'
   }
 
-  this.makeRequest(method, docId, doc, callback)
+  this.make_request(method, doc_id, doc, callback)
 }
