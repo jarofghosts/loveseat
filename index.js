ea7265ac1613eeaff5b0fa289cb781cfc9f6a655
jesse keane <me@jessekeane.me>
2014-04-30 21:12:19 -0700

    derp

--- index.js
+++ index.js
@@ -41,19 +41,19 @@ function Loveseat$make_request(method, url, data, callback) {
 }
 
 Loveseat.prototype.create = function Loveseat$create(callback) {
-  this.make_request('PUT', '', null, callback)
+  this.makeRequest('PUT', '', null, callback)
 }
 
 Loveseat.prototype.get = function Loveseat$get(doc_id, callback) {
-  this.make_request('GET', doc_id, null, callback)
+  this.makeRequest('GET', doc_id, null, callback)
 }
 
 Loveseat.prototype.destroy = function Loveseat$destroy(doc_id, rev, callback) {
-  this.make_request('DELETE', doc_id, { rev: rev }, callback)
+  this.makeRequest('DELETE', doc_id, { rev: rev }, callback)
 }
 
 Loveseat.prototype.check = function Loveseat$check(callback) {
-  this.make_request('HEAD', '', null, callback)
+  this.makeRequest('HEAD', '', null, callback)
 }
 
 Loveseat.prototype.insert = function Loveseat$insert(doc_id, doc, callback) {
@@ -66,5 +66,5 @@ Loveseat.prototype.insert = function Loveseat$insert(doc_id, doc, callback) {
     method = 'POST'
   }
 
-  this.make_request(method, doc_id, doc, callback)
+  this.makeRequest(method, doc_id, doc, callback)
 }
